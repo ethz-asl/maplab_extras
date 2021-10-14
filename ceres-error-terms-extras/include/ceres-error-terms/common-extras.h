80d930d39c0f65b88509eea7862d8414870c25c1
Andrei Cramariuc <andrei.cramariuc@yahoo.com>
2021-10-15 01:13:43 +0200

    Moved unused extra error terms from maplab

---
+++ ceres-error-terms-extras/include/ceres-error-terms/common-extras.h
@@ -0,0 +1,13 @@
+#ifndef CERES_ERROR_TERMS_COMMON_EXTRAS_H_
+#define CERES_ERROR_TERMS_COMMON_EXTRAS_H_
+
+namespace ceres_error_terms {
+
+namespace visual {
+static const int kUnit3BlockSize = 4;
+static const int kInverseDepthBlockSize = 1;
+}  // namespace visual
+
+}  // namespace ceres_error_terms
+
+#endif  // CERES_ERROR_TERMS_COMMON_EXTRAS_H_
